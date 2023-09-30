d3b75fa5a4be8bc39eb5ea43b46754350358d194
Kevin Correa <Correakevleonel@gmail.com>
2023-09-30 00:02:25 -0300

    Pre-entrega

---
+++ main.js
@@ -0,0 +1,22 @@
+for (let turno = 1; turno <= 7; turno++) {
+    let nombre = prompt("ingrese su nombre").toLowerCase();
+    let apellido = prompt("ingrese su apellido").toLowerCase();
+    
+    if (nombre !== "" && apellido !== ""){
+        alert(`Hola ${nombre} ${apellido}`);
+        }   else {
+         alert ("El nombre y el apellido son requeridos para una mejor experiencia")
+        }
+        
+        let edad = parseInt(prompt("Ingrese su edad"));
+        if (edad < 16 ){
+            alert(`Hola ${nombre} ${apellido}! Puedes ingresar a la sala acompañado de un adulto`);
+        }   else if (edad < 18){
+            alert(`Hola ${nombre} ${apellido}! Puedes ingresar a la sala con autorizacion`);
+        }   else {
+            alert(`Hola ${nombre} ${apellido}! Puedes ingresar a la sala`);
+        }
+
+
+    alert(`Turno: #${turno} Nombre: ${nombre}`)
+}
